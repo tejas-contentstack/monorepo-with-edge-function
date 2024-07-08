572c201a85c50ce3a83287a89aa81dfd7c8354ea
tejas-contentstack <132910969+tejas-contentstack@users.noreply.github.com>
2024-07-08 15:48:35 +0530

    use hostname value directly

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -1,10 +1,9 @@
-export default async function handler(request, env) {
+export default async function handler(request) {
     const url = new URL(request.url);
     const hostname = url.hostname;
-    const blockCrawlingForDomain = env.BLOCK_WEB_CRAWLER_FOR_DOMAIN
     
     // Add header for a specific hostname
-    if (hostname === blockCrawlingForDomain || hostname === 'example.domain.com') {
+    if (hostname === 'monorepo-with--block-crwalers-for-a-domain.contentstackapps.com') {
         const response = await fetch(request);
 
         const modifiedResponse =  new Response(response.body, response)
