ec701e44228f1b946545cd3f7e94d6d4762e0f5f
tejas-contentstack <132910969+tejas-contentstack@users.noreply.github.com>
2024-07-08 15:43:35 +0530

    store the env in a variable and then reference

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -1,12 +1,10 @@
 export default async function handler(request, env) {
     const url = new URL(request.url);
     const hostname = url.hostname;
-  
-    console.log("env")
-    console.log(env)
-    console.log(JSON.parse(env).BLOCK_WEB_CRAWLER_FOR_DOMAIN)
+    const blockCrawlingForDomain = env.BLOCK_WEB_CRAWLER_FOR_DOMAIN
+    
     // Add header for a specific hostname
-    if (hostname === JSON.parse(env).BLOCK_WEB_CRAWLER_FOR_DOMAIN || hostname === 'example.domain.com') {
+    if (hostname === blockCrawlingForDomain || hostname === 'example.domain.com') {
         const response = await fetch(request);
 
         const modifiedResponse =  new Response(response.body, response)
